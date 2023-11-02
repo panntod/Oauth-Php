9ed844062bf75dacc7aef2575ec48ca69314576b
Pandhu Arya <pandhu.munjalindra@gmail.com>
2023-11-03 04:52:30 +0700

    [style] restyle card

--- index.php
+++ index.php
@@ -1,25 +1,40 @@
-<?php 
+<?php
 session_start();
+if (!isset($_SESSION['info']['email'])) {
+   header('location: sign_in.php');
+   exit();
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title>Welcome</title>
-   <link rel="stylesheet" href="style.css">
+   <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
-   <div class="container">
+   <div class="form-card">
       <img src="<?= $_SESSION['info']['picture'] ?>" alt="">
-      <p class="bold">Welcome!</p>
-      <p class="medium"><?= $_SESSION['info']['name'] ?></p>
-      <p class="medium"><?= $_SESSION['info']['email'] ?></p>
-      <p class="medium"><?= $_SESSION['info']['address'] ?></p>
-      <p class="medium"><?= $_SESSION['info']['birthdate'] ?></p>
-      <a href="logout.php" class="btn btn-primary">LogOut</a>
-   </div>   
+      <div class="card">
+         <p>
+            Welcome,<span>
+               <?= $_SESSION['info']['name'] ?>
+            </span>
+         </p>
+         <p>
+            Email: <span>
+               <?= $_SESSION['info']['email'] ?>
+            </span>
+         </p>
+         <a href="logout.php" class="logout">LogOut</a>
+      </div>
+
+   </div>
 </body>
+
 </html>
